ea4b6b5132a0c41bc755dd97cf1fc62fce74e66a
Koi Kuo/WHQ/Wistron <koi_kuo@wistron.com>
2023-04-12 17:17:12 +0800

    add logPlugin module

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
-const buttonEle = document.getElementById("button");
+require('./logPlugin'); // 要用node 啟
 
-buttonEle.onclick = function () {
-  import("./test").then((module) => {
-    const print = module.default;
-    print();
-  });
-};
+
+// // --------------用webpack啟動---------------------
+// const buttonEle = document.getElementById("button");
+
+// buttonEle.onclick = function () {
+//   import("./test").then((module) => {
+//     const print = module.default;
+//     print();
+//   });
+// };
+
+
