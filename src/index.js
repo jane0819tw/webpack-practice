464ff99d3bc3add4781ed764e3f818171d45accd
Koi Kuo/WHQ/Wistron <koi_kuo@wistron.com>
2023-04-19 11:01:02 +0800

    change folder name:  pages to containers

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./pages/App.js";
+import App from "./containers/App/App";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
